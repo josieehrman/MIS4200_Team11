0c88b5c5c6e06141a4bddef22ac8ba534694780f
Gce99 <ge052417@ohio.edu>
2022-03-02 12:32:28 -0500

    Added Profiles

---
+++ MIS4200_Team11/Controllers/ProfilesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MIS4200_Team11.DAL;
+using MIS4200_Team11.Models;
+using Microsoft.AspNet.Identity;
+
+namespace MIS4200_Team11.Controllers
+{
+    [Authorize]
+    public class ProfilesController : Controller
+    {
+        private Team11Context db = new Team11Context();
+
+        // GET: Profiles
+        public ActionResult Index()
+        {
+            return View(db.profiles.ToList());
+        }
+
+        // GET: Profiles/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = db.profiles.Find(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // GET: Profiles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profiles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,firstName,lastName,email,businessUnit,hireDate,title")] Profile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                // profile.ID = Guid.NewGuid();
+                Guid newProfile;
+                Guid.TryParse(Profile.Identity.ProfileId(), out newProfile);
+                Profile.ID = newProfile;
+                db.profiles.Add(profile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(profile);
+        }
+
+        // GET: Profiles/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = db.profiles.Find(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // POST: Profiles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,firstName,lastName,email,businessUnit,hireDate,title")] Profile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(profile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(profile);
+        }
+
+        // GET: Profiles/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = db.profiles.Find(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // POST: Profiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Profile profile = db.profiles.Find(id);
+            db.profiles.Remove(profile);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MIS4200_Team11/Models/ProfileModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class ProfileModels
+{
+	public ProfileModels()
+	{
+
+        public Guid ID { get; set; }
+    
+        public string firstName { get; set; }
+    
+        public string lastName { get; set; }
+    
+        public string email { get; set; }
+}
+}
